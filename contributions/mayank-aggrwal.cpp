d358905ee2092255b2940b23c16b3af0d2b210d2
ronwon <aggarwal.mayank49@gmail.com>
2019-10-19 18:01:29 +0530

    Added my contribution for the repository

---
+++ contributions/mayank-aggrwal.cpp
@@ -0,0 +1,9 @@
+
+#include <iostream>
+using namespace std;
+
+int main() {
+
+    cout << "The learning never stops in coding. There is always a new coding technique to learn that will help you solve harder problems. There is always an existing solution that you can enhance. For those who like to learn, this makes coding a very attractive hobby for me." << endl;
+
+}
